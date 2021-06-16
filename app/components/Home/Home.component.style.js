c79657971464db23fd95b7ca94bb0ea64e3457fe
Kimhak Loem <loemkimhak@gmail.com>
2021-06-16 16:33:00 +0900

    R4 - Creating basic components

---
+++ app/components/Home/Home.component.style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});

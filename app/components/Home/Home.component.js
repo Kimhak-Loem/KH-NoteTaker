3453d08f124483f570d76fc628ae13ad994b95fe
Kimhak Loem <loemkimhak@gmail.com>
2021-06-17 12:03:47 +0900

    R11 - Common/Mixin styles

--- app/components/Home/Home.component.js
+++ app/components/Home/Home.component.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput} from 'react-native';
 import styles from './Home.component.style';
 import TextArea from '../TextArea/TextArea.component';
 
 class Home extends Component {
+  state = {
+    title: '',
+  };
+  setTitle = title => this.setState({title});
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.textAreaTitle}> Please enter your note here</Text>
+        <Text style={styles.titleHeading}>Note Title</Text>
+        <TextInput
+          style={styles.titleTextInput}
+          onChangeText={this.setTitle}
+          value={this.state.title}
+        />
+        <Text style={styles.textAreaTitle}>Please enter your note below </Text>
         <TextArea style={styles.textArea} />
       </View>
     );
--- app/components/Home/Home.component.style.js
+++ app/components/Home/Home.component.style.js
@@ -1,23 +1,23 @@
 import {StyleSheet} from 'react-native';
 import theme from '../../styles/theme.style';
+import {headingText, textInput} from '../../styles/common.style';
 
 export default StyleSheet.create({
   container: {
-    flex: 1,
-    paddingVertical: theme.CONTAINER_PADDING,
     alignItems: 'center',
   },
+  titleHeading: {
+    ...headingText,
+  },
+  titleTextInput: {
+    ...textInput,
+  },
   textAreaTitle: {
-    fontSize: theme.FONT_SIZE_MEDIUM,
-    fontWeight: theme.FONT_WEIGHT_BOLD,
-    alignSelf: 'flex-start',
-    padding: 10,
+    ...headingText,
+    fontWeight: theme.FONT_WEIGHT_LIGHT,
+    fontStyle: 'italic',
   },
   textArea: {
-    flex: 1,
-    padding: theme.CONTAINER_PADDING,
-    alignSelf: 'stretch',
-    overflow: 'scroll',
-    backgroundColor: theme.BACKGROUND_COLOR_LIGHT,
+    ...textInput,
   },
 });
